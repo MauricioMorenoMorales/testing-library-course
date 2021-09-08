50dbab0216492573a8e72b02546376c97f9985ed
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-08 16:26:59 -0500

    Testing Functionality with Fire Event

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Counter.css';
 
 const Counter = () => {
+	const [counterValue, setCounterValue] = useState(0);
+	const [inputValue, setInputValue] = useState(1);
 	return (
 		<div>
 			<h1 data-testid="header">My Counter</h1>
+			<h2 data-testid="counter">{counterValue}</h2>
+			<button data-testid="subtract-button">-</button>
+			<input
+				data-testid="input"
+				type="number"
+				value={inputValue}
+				className="text-center"
+			/>
+			<button data-testid="add-button">+</button>
 		</div>
 	);
 };
 
 export default Counter;
+
+// 38:00
--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -10,6 +10,31 @@ test('Heade renders with correct text', () => {
 	// const headerEl = component.getByTestId('header');
 	const { getByTestId } = render(<Counter />);
 	const headerEl = getByTestId('header');
-
 	expect(headerEl.textContent).toBe('My Counter');
+});
+
+test('Counter initially start with value of 0 ', () => {
+	const { getByTestId } = render(<Counter />);
+	const counterEl = getByTestId('counter');
+	expect(counterEl.textContent).toBe('0');
+});
+
+test('Input contains initial value of 1.', () => {
+	const { getByTestId } = render(<Counter />);
+	const inputEl = getByTestId('input');
+	expect(inputEl.value).toBe('1');
+});
+
+test('Add button renders with +', () => {
+	const { getByTestId } = render(<Counter />);
+	const addButton = getByTestId('add-button');
+
+	expect(addButton.textContent).toBe('+');
+});
+
+test('Subtract button renders with +', () => {
+	const { getByTestId } = render(<Counter />);
+	const subtractButton = getByTestId('subtract-button');
+
+	expect(subtractButton.textContent).toBe('-');
 });
